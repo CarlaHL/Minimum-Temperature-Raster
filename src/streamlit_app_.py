42a01841a96348f5a6cf7910072b17d2318652a9
CarlaHL <155692380+CarlaHL@users.noreply.github.com>
2025-10-18 18:17:41 -0500

    Update streamlit_app_.py

--- src/streamlit_app_.py
+++ src/streamlit_app_.py
@@ -1,23 +1,26 @@
 import streamlit as st
 
-st.set_page_config(page_title="Geospatial Analysis of Hospitals in Peru", layout="wide")
+st.set_page_config(page_title="Peru Minimum Temperature (Tmin) Raster Analysis", layout="wide")
 
-tab1, tab2, tab3 = st.tabs(["Data Desctription", "Static Maps & Department Analysis", "Dynamic Maps"])
+tab1, tab2, tab3 = st.tabs(["Desctription", "Analysis & Visualizations", "Public policy"])
 
 with tab1:
 
      st.markdown(
 """
-# Data Desctription
-The purpose of this application is to present a geospatial analysis of hospitals in Peru, both at the departmental level and at the population center level. Through this visualization, we aim to identify patterns in their geographic distribution as well as potential gaps in access to healthcare services.
+# Desctription
+The purpose of this application is to present a geospatial analysis of minimum temperature (Tmin) data across Peru using satellite-derived GeoTIFF raster layers. By calculating zonal statistics (mean, min, max, standard deviation, percentiles, etc.) for each district, we aim to:
+- Identify cold zones with increased risk of frost and cold surges.
+- Support public decision-making by highlighting vulnerable areas.
+- Propose evidence-based policies for risk management and climate adaptation.
 
-
-Our code is hosted in the following GithHub repository: https://github.com/CarlaHL/Hospitals-Access-Peru
+The analysis is built using Python, GeoPandas, raster statistics, and climate data. 
+Our code is hosted in the following GithHub repository: https://github.com/CarlaHL/Minimum-Temperature-Raster
 """
      )
 
 with tab2:
-     st.markdown("# Static Maps & Department Analysis")
+     st.markdown("# Analysis & Visualizations")
     
      import streamlit as st
      import matplotlib.pyplot as plt
@@ -43,12 +46,4 @@ with tab2:
      st.image("assets/hospitals_dept_map.png", use_container_width=True)
 
 with tab3:
-    st.markdown("# Dynamic Maps")
-    st.subheader("Lima")
-    with open("assets/mapa_lima.html", "r", encoding="utf-8") as f:
-        components.html(f.read(), height=600)
-
-    st.subheader("Loreto")
-    with open("assets/mapa_loreto.html", "r", encoding="utf-8") as f:
-        components.html(f.read(), height=600)
-
+    st.markdown("# Public policy")
